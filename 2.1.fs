151f877b5852ca040aa3acd7da79caed01177594
Талян <70955135+Talyan-Deboshir@users.noreply.github.com>
2025-03-05 23:32:30 +0500

    Add files via upload

---
+++ 2.1.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let rim_to_des rim =
+    match rim with
+    | "I" -> 1
+    | "II" -> 2
+    | "III" -> 3
+    | "IV" -> 4
+    | "V" -> 5
+    | "VI" -> 6
+    | "VII" -> 7
+    | "VIII" -> 8
+    | "IX" -> 9
+
+
+let rims = ["I"; "II"; "III"; "IV"; "V"; "VI"; "VII"; "VIII"; "IX"]
+
+let desNum = List.map rim_to_des rims
+
+printfn "Десятичные: %A" desNum
