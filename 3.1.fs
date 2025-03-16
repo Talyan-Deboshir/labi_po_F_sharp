bc0f2301ef059b04c2f3010fb24715015f1e6668
Талян <70955135+Talyan-Deboshir@users.noreply.github.com>
2025-03-16 13:57:30 +0500

    Add files via upload

---
+++ 3.1.fs
@@ -0,0 +1,44 @@
+﻿open System
+
+let rims_to_des (roman: string) : Option<int> =
+    match roman with
+    | "I" -> Some 1
+    | "II" -> Some 2
+    | "III" -> Some 3
+    | "IV" -> Some 4
+    | "V" -> Some 5
+    | "VI" -> Some 6
+    | "VII" -> Some 7
+    | "VIII" -> Some 8
+    | "IX" -> Some 9
+    | _ -> None
+
+let rec lazy_string_seq () : seq<int> =
+    printfn "Введите римские цифры (I - IX) через Enter (пустая строка для завершения):"
+    
+    let rec read_seq () : seq<Lazy<string>> =
+        seq {
+            let input = lazy (printfn "ввод "; Console.ReadLine())
+            if not (input.Value = null || input.Value = "") then
+                yield input
+                yield! read_seq ()
+        }
+    
+    let des_from_rims: seq<Lazy<Option<int>>> = 
+        read_seq () 
+        |> Seq.map (fun lz -> lazy (printfn "преобразование "; rims_to_des lz.Value)) 
+        |> Seq.cache 
+
+    if Seq.exists (fun (lz: Lazy<Option<int>>) -> Option.isNone lz.Value) des_from_rims then
+        printfn "Введены некорректные римские цифры, попробуйте снова."
+        lazy_string_seq ()
+    else
+        des_from_rims |> Seq.choose (fun (lz: Lazy<Option<int>>) -> lz.Value) |> Seq.cache 
+
+[<EntryPoint>]
+let main argv =
+    let des_nums = lazy_string_seq ()
+    printf "Десятичные цифры: "
+    des_nums |> Seq.iter (printf "%d ")
+    printfn ""
+    0
---
+++ 3.3.fs
@@ -0,0 +1,35 @@
+﻿open System
+open System.IO
+
+
+let rec read_files (dir: string) : Lazy<seq<string>> =
+    lazy seq {
+        yield! Directory.EnumerateFiles(dir)
+        for subdir in Directory.EnumerateDirectories(dir) do
+            yield! (read_files subdir).Value
+    }
+
+
+let get_extension (dir: string) : Lazy<option<string * int>> =
+    lazy (
+        read_files dir
+        |> fun lazyFiles -> lazyFiles.Value
+        |> Seq.map Path.GetExtension
+        |> Seq.filter (fun ext -> ext <> "" && ext |> Seq.exists (fun c -> c <> ' '))
+        |> Seq.countBy id
+        |> Seq.sortBy snd
+        |> Seq.tryHead
+    )
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите путь к каталогу: "
+    let dir = Console.ReadLine()
+
+    let min_extension = get_extension dir
+
+    match min_extension.Value with
+    | Some (ext, count) -> printfn "Наименее частое расширение: %s (встречается %d раз)" ext count
+    | None -> printfn "В указанной директории нет файлов с расширениями"
+
+    0
