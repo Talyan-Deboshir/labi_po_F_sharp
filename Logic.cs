0bfca86ea9ca800daa3136ae4bf4faab87c1247f
Талян <70955135+Talyan-Deboshir@users.noreply.github.com>
2025-04-10 00:59:02 +0500

    Add files via upload

---
+++ Logic.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab
+{
+    public class Logic
+    {
+        private bool haveJob;
+        private bool haveRussianCitizenship;
+
+        public bool HaveJob
+        {
+            get { return haveJob; }
+            set { haveJob = value; }
+        }
+
+        public bool HaveRussianCitizenship
+        {
+            get { return haveRussianCitizenship; }
+            set { haveRussianCitizenship = value; }
+        }
+
+        public Logic()
+        {
+            HaveJob = false;
+            HaveRussianCitizenship = false;
+        }
+
+        public Logic(bool haveJob, bool haveRussianCitizenship)
+        {
+            HaveJob = haveJob;
+            HaveRussianCitizenship = haveRussianCitizenship;
+        }
+
+        public Logic(Logic other)
+        {
+            HaveJob = other.HaveJob;
+            HaveRussianCitizenship = other.HaveRussianCitizenship;
+        }
+
+        public bool Equivalent()
+        {
+            return HaveJob == HaveRussianCitizenship;
+        }
+
+        public override string ToString()
+        {
+            return $"HaveJob: {HaveJob}, HaveRussianCitizenship: {HaveRussianCitizenship}";
+        }
+    }
+}
