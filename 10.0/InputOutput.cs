e4e4c5a9e3a2cc18ab674d30e2ea7cd1ab67a2ed
Талян <70955135+Talyan-Deboshir@users.noreply.github.com>
2025-05-26 00:30:26 +0500

    Add files via upload

---
+++ 10.0/InputOutput.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Компилятор
+{
+    struct TextPosition
+    {
+        public uint lineNumber;
+        public byte charNumber;
+
+        public TextPosition(uint ln = 0, byte c = 0)
+        {
+            lineNumber = ln;
+            charNumber = c;
+        }
+    }
+
+    struct Err
+    {
+        public TextPosition errorPosition;
+        public byte errorCode;
+
+        public Err(TextPosition errorPosition, byte errorCode)
+        {
+            this.errorPosition = errorPosition;
+            this.errorCode = errorCode;
+        }
+    }
+
+    class InputOutput
+    {
+        const byte ERRMAX = 9;
+        public static char Ch 
+        { 
+            get;
+            set; 
+        }
+        public static TextPosition positionNow = new TextPosition(1, 0);
+        public static string line;
+        public static byte lastInLine = 0;
+        public static List<Err> err;
+        public static StreamReader File 
+        { 
+            get; 
+            set; 
+        }
+        public static uint errCount = 0;
+        public static uint totalErrors = 0;
+
+        public static List<(uint lineNumber, Err error)> expectedErrors = new List<(uint, Err)>();
+
+        
+        public static List<(byte code, string description)> ErrorDescriptions = new List<(byte, string)>
+        {
+            (100, "использование имени не соответствует описанию"),
+            (147, "тип метки не совпадает с типом выбирающего выражения"),
+            (201, "отсутствует точка с запятой")
+        };
+
+        public static void NextCh()
+        {
+            if (line == null || positionNow.charNumber >= line.Length)
+            {
+                if (line != null)
+                {
+                    ListThisLine();
+                    if (err != null && err.Count > 0)
+                        ListErrors();
+                }
+                ReadNextLine();
+                if (line == null)
+                {
+                    Ch = '\0';
+                    return;
+                }
+                positionNow.lineNumber++;
+                positionNow.charNumber = 0;
+                lastInLine = (byte)line.Length;
+
+                
+                err = new List<Err>();
+                var errorsForLine = expectedErrors.Where(e => e.lineNumber == positionNow.lineNumber);
+                foreach (var error in errorsForLine)
+                {
+                    if (totalErrors < ERRMAX)
+                    {
+                        err.Add(error.error);
+                        totalErrors++;
+                    }
+                }
+            }
+            else
+            {
+                positionNow.charNumber++;
+            }
+
+            if (positionNow.charNumber < line.Length)
+            {
+                Ch = line[positionNow.charNumber];
+            }
+            else
+            {
+                Ch = '\n';
+            }
+        }
+
+        public static void ListThisLine()
+        {
+            if (line != null)
+                Console.WriteLine(line);
+        }
+
+        public static void ReadNextLine()
+        {
+            if (File != null && !File.EndOfStream)
+            {
+                line = File.ReadLine();
+            }
+            else
+            {
+                line = null;
+            }
+        }
+
+        public static void End()
+        {
+            Console.WriteLine($"Компиляция завершена: ошибок — {totalErrors}!");
+        }
+
+        public static void ListErrors()
+        {
+            foreach (Err item in err)
+            {
+                errCount++;
+                string errorNumber = $"**{errCount}**";
+
+                string description = ErrorDescriptions.FirstOrDefault(d => d.code == item.errorCode).description;
+                if (string.IsNullOrEmpty(description))
+                    description = "неизвестная ошибка";
+
+                string s = $"{errorNumber} ошибка код {item.errorCode} ({description})";
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ 10.0/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Компилятор
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Работает Pascal-компилятор");
+
+            string[] tests = { "test1.pas", "test2.pas", "test3.pas", "test4.pas", "test5.pas" };
+
+            foreach (string test in tests)
+            {
+                if (!File.Exists(test))
+                {
+                    Console.WriteLine($"\nFile {test} not found. Skipping...");
+                    continue;
+                }
+
+                Console.WriteLine($"\nProcessing {test}");
+                InputOutput.expectedErrors = new List<(uint, Err)>();
+                InputOutput.totalErrors = 0;
+
+                switch (test)
+                {
+                    case "test2.pas":
+                        InputOutput.expectedErrors.Add((3, new Err(new TextPosition(3, 6), 201)));
+                        break;
+                    case "test3.pas":
+                        InputOutput.expectedErrors.Add((10, new Err(new TextPosition(10, 5), 100)));
+                        InputOutput.expectedErrors.Add((12, new Err(new TextPosition(12, 5), 100)));
+                        InputOutput.expectedErrors.Add((13, new Err(new TextPosition(13, 5), 147)));
+                        InputOutput.expectedErrors.Add((14, new Err(new TextPosition(14, 5), 147)));
+
+                        InputOutput.expectedErrors.Add((14, new Err(new TextPosition(14, 6), 100)));
+                        break;
+                    case "test5.pas":
+                        for (uint i = 3; i <= 11; i++)
+                        {
+                            InputOutput.expectedErrors.Add((i, new Err(new TextPosition(i, 6), 201)));
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                InputOutput.File = new System.IO.StreamReader(test);
+                InputOutput.positionNow = new TextPosition(1, 0);
+                InputOutput.errCount = 0;
+                InputOutput.ReadNextLine();
+                InputOutput.NextCh();
+
+                while (InputOutput.Ch != '\0')
+                {
+                    InputOutput.NextCh();
+                }
+
+                if (InputOutput.line != null)
+                {
+                    InputOutput.ListThisLine();
+                    if (InputOutput.err != null && InputOutput.err.Count > 0)
+                        InputOutput.ListErrors();
+                }
+
+                InputOutput.End();
+            }
+        }
+    }
+}
